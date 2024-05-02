12d3c947d92156c322fabafb099c36fd06d6e8cc
sridhara-sundar <sridhara.sundar@chatbot.team>
2024-05-02 17:01:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+import ChatWidget from './components/ChatWidget';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Define a route for your ChatWidget component */}
+        <Route path="/chat-widget/:token1/:prop1" element={<ChatWidgetWrapper />} />
+      </Routes>
+    </Router>
+  );
+}
+
+// Wrapper component to extract URL parameters and pass them to ChatWidget
+const ChatWidgetWrapper = () => {
+  const { token1, prop1 } = useParams();
+  console.log("props1:", token1);
+  console.log("props2:", prop1);
+  
+  return <ChatWidget token1={token1} prop1={prop1} />;
+};
+
+export default App;
---
+++ src/components/ChatWidget.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Widget, addResponseMessage, addLinkSnippet, addUserMessage, setQuickButtons } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
+import ChatWidgetLogo from './logo-small.png';
+import HeaderImage from './logo-dark.png';
+import { useParams } from 'react-router-dom';
+
+const buttons = [{ label: 'first', value: '1' }, { label: 'second', value: '2' }];
+const ChatWidget = (props) => {
+    // const { prop1, prop2 } = useParams();
+    useEffect(() => {
+        addResponseMessage('Welcome to this awesome chat!');
+        setQuickButtons(buttons);
+        console.log("token1:", props.token1);
+        console.log("props1:", props.prop1);
+    }, []);
+
+    const handleNewUserMessage = (newMessage) => {
+        console.log(`New message incoming! ${newMessage}`);
+        addResponseMessage('Thank you for contact!');
+        // Now send the message throught the backend API
+    };
+
+    const handleQuickButtonClicked = (data) => {
+        console.log(data);
+        setQuickButtons(buttons.filter(button => button.value !== data));
+    };
+
+    return (
+        <div className="ChatWidget">
+            {
+                // props.token1 === "6371968684" &&
+
+                <Widget
+                    handleNewUserMessage={handleNewUserMessage}
+                    handleQuickButtonClicked={handleQuickButtonClicked}
+                    profileAvatar={ChatWidgetLogo}
+                    title='DEYOR'
+                    subtitle="Chatbot.Team"
+                />
+            }
+        </div>
+    );
+
+}
+
+
+export default ChatWidget;
